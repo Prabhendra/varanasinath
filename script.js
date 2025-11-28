730c40c3100368a7f0b18347e352cf997f121146
prabhendra <p786solot@gmail.com>
2025-11-28 14:03:51 +0530

    Navbar fixed, runway removed, scroll enabled

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("varanasinath.com loaded");
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        // Scroll down → white navbar
+        navbar.classList.remove("bg-transparent", "text-white");
+        navbar.classList.add("bg-white", "text-black", "shadow-md");
+    } else {
+        // Reset to transparent
+        navbar.classList.add("bg-transparent", "text-white");
+        navbar.classList.remove("bg-white", "text-black", "shadow-md");
+    }
+});
